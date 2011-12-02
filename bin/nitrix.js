88194c4d87043b6b637a3b6c9f8247ff576cb311
Edward Hotchkiss <e@ingk.com>
2011-12-02 09:35:30 -0800

    test server, spawn

--- bin/nitrix.js
+++ bin/nitrix.js
@@ -5,12 +5,12 @@ var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 var path = require('path');
 
-var args = process.argv.splice(2);
+var args = process.argv.slice(2);
+var app = args[0];
 
 // start node myappXYZ.js
-function start() {
-  var app = args[1];
-  node = spawn('node', app);
+function start(app) {
+  node = spawn('node', [app]);
   node.stdout.on('data', function(data){
     console.log(data);
   });
@@ -20,13 +20,21 @@ function start() {
   node.on('exit', function(code, signal){
     start();
   });
+  node.stderr.on('data', function (data) {
+    if (/^execvp\(\)/.test(data)) {
+      console.log('Failed to start child process.');
+    }
+  });
 };
 
 // listen for uncaughtExceptions
 process.on('uncaughtException', function(error){
-  console.error('App error, restarting --');
+  console.error('App error, restarting!');
   console.error(error.stack);
   start();
 });
+
+// switch
+start(app);
 
 /* EOF */
