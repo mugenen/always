f6bae4c61307103d0876bd183bf5e2abaec582fa
Edward Hotchkiss <e@ingk.com>
2011-12-09 13:08:25 -0500

    nitrix test suite started for the cli

---
+++ test/cli.test.js
@@ -0,0 +1,46 @@
+
+/*!
+  Core Modules
+ */
+
+var fs = require('fs'),
+    vows = require('vows'),
+    assert = require('assert');
+
+/*!
+  Setup
+ */
+
+var testApp =
+  'var http = require(\'http\');\n'+
+  'var app = http.createServer(function(request, response) {\n'+
+  '  response.end(\'hi\');\n'+
+  '});\n'+
+  'app.listen(8000)';
+
+/*!
+  Vows
+ */
+
+vows.describe('nitrix vows setup & teardown').addBatch({
+  'when creating file /test/app.js':{
+    topic:function(){
+      fs.writeFile(__dirname+'/app.js', testApp, 'utf8', this.callback);
+    },
+    'there should be no errors':function(error){
+      assert.equal(typeof(error), 'undefined');
+    }
+  }
+}).addBatch({
+  'when removing /test/app.js':{
+    topic:function(){
+      fs.unlink(__dirname+'/app.js', this.callback);
+    },
+    'there should be no errors':function(error) {
+      assert.equal(typeof(error), 'undefined');
+    }
+  }
+})
+.export(module);
+
+/* EOF */
