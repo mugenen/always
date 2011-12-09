6a37d14e0420aceace7e33dfbb060b608e1c089e
Edward Hotchkiss <edward@edwardhotchkiss.com>
2011-12-09 12:34:06 -0500

    setup for automated tests

--- test/nitrix.test.js
+++ test/nitrix.test.js
@@ -1,8 +1,15 @@
 
-var vows = require('vows');
-var assert = require('assert');
+var vows = require('vows'),
+    assert = require('assert'),
+    nitrix = require('../bin/nitrix');
 
-var nitrix = require('../bin/nitrix');
+// setup
+var testApp = '
+  var http = require("http"),\n'+
+  '  app = http.createServer(function(request, response) {\n'+
+  '    response.end('hi');\n'+
+  '  });\n'+
+  'app.listen(8000)';
 
 vows.describe('nitrix.typeof').addBatch({
   'when checking the typeof of nitrix':{
